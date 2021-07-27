54f5479ecc45b6a1eb2635418e019719b179852a
KayPea <prescottdarling@gmail.com>
2021-07-27 13:29:03 -0400

    added context to server.js, notes/links to apollo docs in here.

--- server.js
+++ server.js
@@ -8,6 +8,23 @@ const resolvers = require("./graphql/resolvers");
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: (ctx) => ctx,
+  /*
+  // context: (integrationContext) => ({
+  //   authScope: getScope(integrationContext.req.headers.authorization),
+  // }),
+    !Important: The 'integrationContext' arg varies depending on the specific integration(eg Express/Koa/Lambda etc)being used. (See table for specific signitures.):
+      Express (including apollo-server)	{
+        req: express.Request,
+        res: express.Response
+      }
+    https://www.apollographql.com/docs/apollo-server/api/apollo-server/#context
+    https://www.apollographql.com/docs/apollo-server/data/resolvers/#the-context-argument
+
+    * context: An object (or a function that creates an object) that's passed to every resolver that executes for a particular operation. This enables resolvers to share helpful context, such as a database connection.
+    
+    Certain fields are added to this object automatically, depending on which Node.js middleware your server uses.
+  */
 });
 
 server.listen().then(({ url }) => {
